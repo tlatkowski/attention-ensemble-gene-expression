38f4389a60bc8a811abeea2adc57ba7af73fca21
Tomasz Latkowski <t.latkowski@gmail.com>
2017-10-05 21:59:15 +0200

    Removed unused functions.

--- nn_utils.py
+++ nn_utils.py
@@ -9,41 +9,6 @@ def norm_data(X: pd.DataFrame):
     return X_norm
 
 
-def init_weights(num_features, num_methods = 4):
-    parameters = dict()
-    for i in range(1, num_methods + 1):
-        parameters['W' + str(i)] = tf.get_variable('W' + str(i),
-                                                   shape=[150, num_features],
-                                                   initializer=tf.contrib.layers.xavier_initializer())
-        parameters['b' + str(i)] = tf.get_variable('b' + str(i),
-                                                   shape=[150, 1],
-                                                   initializer=tf.zeros_initializer)
-        parameters['W' + str(i) + '_2'] = tf.get_variable('W' + str(i) + '_2',
-                                                         shape=[1, 150],
-                                                         initializer=tf.contrib.layers.xavier_initializer())
-        parameters['b' + str(i) + '_2'] = tf.get_variable('b' + str(i) + '_2',
-                                                         shape=[1, 1],
-                                                         initializer=tf.zeros_initializer)
-    return parameters
-
-
-def forward_pass(parameters, inputs):
-    m = len(parameters) // 4
-    f_pass = dict()
-    for i in range(1, m+1):
-        f_pass['A' + str(i)] = tf.nn.tanh(
-            tf.matmul(
-                parameters['W' + str(i)],
-                inputs['X' + str(i)])
-            + parameters['b' + str(i)])
-        f_pass['A' + str(i) + '_2'] = tf.nn.sigmoid(
-            tf.matmul(
-                parameters['W' + str(i) + '_2'],
-                f_pass['A' + str(i)])
-            + parameters['b' + str(i) + '_2'])
-    return f_pass
-
-
 def init_inputs(num_features, selection_methods):
     inputs = dict()
     for selection_method in selection_methods:
