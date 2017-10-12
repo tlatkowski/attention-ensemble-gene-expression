6029694c95e2ff5f7c8b996797cd5e7798cfb7ba
Tomasz Latkowski <t.latkowski@gmail.com>
2017-10-12 21:36:25 +0200

    added names for tensorboard graph.

--- nn_utils.py
+++ nn_utils.py
@@ -12,19 +12,19 @@ def norm_data(X: pd.DataFrame):
 def init_inputs(num_features, selection_methods):
     inputs = dict()
     for selection_method in selection_methods:
-        inputs[selection_method] = tf.placeholder(dtype=tf.float32, shape=[None, num_features], name=selection_method)
+        inputs[selection_method] = tf.placeholder(dtype=tf.float32, shape=[None, num_features], name=selection_method + '_input')
     return inputs
 
 
 def dense_feed_forward(nn_inputs: dict, units, activation):
     dense_ff = dict()
     for name, nn_input in nn_inputs.items():
-        dense_ff[name] = tf.layers.dense(nn_input, units=units, activation=activation)
+        dense_ff[name] = tf.layers.dense(nn_input, units=units, activation=activation, name=name + '_ff')
     return dense_ff
 
 
 def logits_layer(feed_forward, units):
     logits_layers = dict()
     for name, ff in feed_forward.items():
-        logits_layers[name] = tf.layers.dense(ff, units=1)
+        logits_layers[name] = tf.layers.dense(ff, units=units, name=name + '_logits')
     return logits_layers
